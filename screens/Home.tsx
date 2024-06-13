c554236df2e2bd578b0e37077b1a595e5c7316ef
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-06-13 22:23:39 +0300

    Added Cards Section UI With DUMMY_DATA In Home Screen

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,15 +1,53 @@
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
-import { Text, Button, View, TouchableOpacity, Image } from "react-native";
+import React, { useRef } from "react";
+import {
+  Text,
+  Button,
+  View,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import tw from "tailwind-react-native-classnames";
 import { useAuth } from "../contexts/AuthContext";
-import { Ionicons } from "@expo/vector-icons";
+import { Entypo, Ionicons } from "@expo/vector-icons";
+import Swiper from "react-native-deck-swiper";
+
+const DUMMY_DATA = [
+  {
+    displayName: "Anton Jeejo",
+    job: "Software Engineer",
+    photoURL:
+      "https://pbs.twimg.com/profile_images/1540318789061197825/RiJ0V1sR_400x400.jpg",
+    age: 23,
+    id: 1,
+  },
+  {
+    displayName: "Mark Zuckerberg",
+    job: "Programmer",
+    photoURL:
+      "https://upload.wikimedia.org/wikipedia/commons/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg",
+    age: 39,
+    id: 2,
+  },
+  {
+    displayName: "Justin Mateen",
+    job: "Software Developer",
+    photoURL:
+      "https://i.insider.com/606730e3856cd700198a2dd1?width=1136&format=jpeg",
+    age: 37,
+    id: 3,
+  },
+];
+
 const Home = () => {
   const navigation = useNavigation<any>();
   const { logout } = useAuth();
+  const swipeRef = useRef<any>(null);
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={tw.style("flex-1 mt-6")}>
       {/* Header  */}
       <View style={tw.style("flex-row items-center justify-between px-5")}>
         <TouchableOpacity onPress={logout}>
@@ -37,8 +75,128 @@ const Home = () => {
         </TouchableOpacity>
       </View>
       {/* End Header  */}
+      {/* Cards  */}
+      <View style={tw.style("flex-1 -mt-6")}>
+        <Swiper
+          ref={swipeRef}
+          containerStyle={{
+            backgroundColor: "transparent",
+          }}
+          cards={DUMMY_DATA}
+          stackSize={5}
+          cardIndex={0}
+          animateCardOpacity
+          verticalSwipe={false}
+          onSwipedLeft={(cardIndex) => {
+            console.log("Swipe Pass", cardIndex);
+          }}
+          onSwipedRight={(cardIndex) => {
+            console.log("Swipe Match", cardIndex);
+          }}
+          backgroundColor="#4FD0E9"
+          overlayLabels={{
+            left: {
+              title: "NOPE",
+              style: {
+                label: {
+                  textAlign: "right",
+                  color: "red",
+                },
+              },
+            },
+            right: {
+              title: "MATCH",
+              style: {
+                label: {
+                  color: "#4DED30",
+                },
+              },
+            },
+          }}
+          renderCard={(card) => {
+            return card ? (
+              <View
+                key={card.id}
+                style={tw.style("bg-white h-3/4 rounded-xl relative")}
+              >
+                <Image
+                  style={tw.style("absolute top-0 h-full w-full rounded-xl")}
+                  source={{ uri: card.photoURL }}
+                />
+
+                <View
+                  style={[
+                    tw.style(
+                      "absolute bottom-0 bg-white w-full h-20 justify-between items-center flex-row px-6 py-2 rounded-b-xl shadow-xl"
+                    ),
+                    styles.cardShadow,
+                  ]}
+                >
+                  <View>
+                    <Text style={tw.style("text-xl font-bold")}>
+                      {card.displayName}
+                    </Text>
+                    <Text>{card.job}</Text>
+                  </View>
+                  <Text style={tw.style("text-2xl font-bold")}>{card.age}</Text>
+                </View>
+              </View>
+            ) : (
+              <View
+                style={tw.style(
+                  "relative bg-white h-3/4 rounded-xl justify-center items-center shadow-xl"
+                )}
+              >
+                <Text style={tw.style("font-bold pb-5")}>No more profiles</Text>
+                <Image
+                  style={tw.style("h-20 w-20")}
+                  height={100}
+                  width={100}
+                  source={{
+                    uri: "https://cdn.shopify.com/s/files/1/1061/1924/products/Crying_Face_Emoji_large.png?v=1571606037",
+                  }}
+                />
+              </View>
+            );
+          }}
+        />
+      </View>
+      {/* End Cards  */}
+
+      <View style={tw.style("flex flex-row justify-evenly")}>
+        <TouchableOpacity
+          onPress={() => swipeRef.current.swipeLeft()}
+          style={tw.style(
+            "items-center justify-center rounded-full w-16 h-16 bg-red-200"
+          )}
+        >
+          <Entypo name="cross" size={24} color="red" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => swipeRef.current.swipeRight()}
+          style={tw.style(
+            "items-center justify-center rounded-full w-16 h-16 bg-green-200"
+          )}
+        >
+          <Entypo name="heart" size={24} color="green" />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  cardShadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
+  },
+});
