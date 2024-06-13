8655641df174ca45882da9dd2344e368265a06eb
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-06-13 21:18:59 +0300

    Added Header Part In Home Screen

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,21 +1,42 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { Text, Button } from "react-native";
+import { Text, Button, View, TouchableOpacity, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import tw from "tailwind-react-native-classnames";
 import { useAuth } from "../contexts/AuthContext";
+import { Ionicons } from "@expo/vector-icons";
 const Home = () => {
   const navigation = useNavigation<any>();
   const { logout } = useAuth();
-
   return (
     <SafeAreaView>
-      <Text style={tw.style("text-blue-400")}>Home Screen</Text>
-      <Button
-        title="Open All Chats"
-        onPress={() => navigation.navigate("Chats")}
-      />
-      <Button title="Logout" onPress={logout} />
+      {/* Header  */}
+      <View style={tw.style("flex-row items-center justify-between px-5")}>
+        <TouchableOpacity onPress={logout}>
+          <Image
+            style={tw.style("h-10 w-10 rounded-full")}
+            source={{
+              uri: "https://img.freepik.com/free-icon/user_318-159711.jpg",
+            }}
+          />
+        </TouchableOpacity>
+
+        <Text
+          style={{
+            textTransform: "uppercase",
+            fontSize: 14,
+            fontWeight: "600",
+            color: "black",
+          }}
+        >
+          Dating App
+        </Text>
+
+        <TouchableOpacity onPress={() => navigation.navigate("Chats")}>
+          <Ionicons name="chatbubbles-sharp" size={30} color="#000000" />
+        </TouchableOpacity>
+      </View>
+      {/* End Header  */}
     </SafeAreaView>
   );
 };
